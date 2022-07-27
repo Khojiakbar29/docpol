4337ddde9ac686dc5a410745821f7debd8a50536
Khojiakbar <kh.tukhtasinov@gmail.com>
2022-07-27 22:14:35 +0100

    feedbacks added

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ const sliderLine = document.querySelector('.feedback__people-wrapper');
 
 document.querySelector('.feedback__buttons-next').addEventListener('click', function() {
     offset = offset + 400; // offset += 500;
-    if (offset > 800) {
+    if (offset > 2800) {
         offset = 0;
     }
     sliderLine.style.left = - offset + 'px';
@@ -20,7 +20,7 @@ document.querySelector('.feedback__buttons-next').addEventListener('click', func
 document.querySelector('.feedback__buttons-prev').addEventListener('click', function() {
     offset = offset - 400; // offset -= 500;
     if (offset < 0) {
-        offset = 800;
+        offset = 2800;
     }
     sliderLine.style.left = - offset + 'px';
 });
